e4fa5fa25e2dea51382dc818c8df79f9c60478fc
Raymond427 <rferrel427@gmail.com>
2019-10-05 09:57:03 -0400

    home viewport height patch

--- src/component/page/home/index.js
+++ src/component/page/home/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ChapterCarousel from './ChapterCarousel'
 import chapters from '../../../data/chapters.json'
+import Div100vh from 'react-div-100vh'
 
-const Home = () => <ChapterCarousel chapters={chapters} />
+const Home = () =>
+    <Div100vh>
+        <ChapterCarousel chapters={chapters} />
+    </Div100vh>
 
 export default Home
