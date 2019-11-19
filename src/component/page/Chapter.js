9a5e76cc6f34ec279d389f57ca8546b6ebcad3f3
JonHaz <jonathanhazeley@gmail.com>
2019-11-18 22:00:35 -0500

    strategy chapter foundation

--- src/component/page/Chapter.js
+++ src/component/page/Chapter.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import chapterImages from '../../data/homePageImages'
-import '../../styles/Chapter.css'
-import Navigation from '../navigation'
-import Picture from '../Picture'
-import Habits from '../chapter/chapterContent/habits'
+import React from "react";
+import chapterImages from "../../data/homePageImages";
+import "../../styles/Chapter.css";
+import Navigation from "../navigation";
+import Picture from "../Picture";
+import Habits from "../chapter/chapterContent/habits";
+import Strategy from "../chapter/chapterContent/strategy";
 
 const Chapter = ({ chapter }) => {
-    const backgroundImgs = chapterImages[chapter.name].chapterBackgroundImages
-    const ChapterContents = chapterName => {
-        switch (chapterName) {
-            case chapterName === 'habits':
-                return <Habits />
-            default:
-                return <Habits />
-        }
+  const backgroundImgs = chapterImages[chapter.name].chapterBackgroundImages;
+  const ChapterContents = chapterName => {
+    switch (chapterName) {
+      case "habits":
+        return <Habits />;
+      case "strategy":
+        return <Strategy />;
+      default:
+        return <Habits />;
     }
+  };
 
-    return (
-        <div className="Chapter">
-            <div className="chapter-container">
-                <Picture className="chapter-background-image" alt="chapter background" images={backgroundImgs} />
-                <div className="chapter-content">
-                    <Navigation showThemeToggle />
-                    {ChapterContents(chapter.name)}
-                </div>
-            </div>
+  return (
+    <div className="Chapter">
+      <div className="chapter-container">
+        <Picture
+          className="chapter-background-image"
+          alt="chapter background"
+          images={backgroundImgs}
+        />
+        <div className="chapter-content">
+          <Navigation showThemeToggle />
+          {ChapterContents(chapter.name)}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Chapter
+export default Chapter;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,47 +1,49 @@
-export const COMPANY_EMAIL = 'admin@advancinginnovativeminds.org'
+export const COMPANY_EMAIL = "admin@advancinginnovativeminds.org";
 
 export const PATHS = {
-    HOME: '/',
-    LOGIN: '/login',
-    SIGN_UP: '/sign-up',
-    USER_MANAGEMENT: '/usermgmt',
-    RESET_PASSWORD: '/reset-password',
-    ACCOUNT: '/account',
-    BUY: '/buy',
-    FEEDBACK: '/feedback',
-    ORDERS: '/orders',
-    CHAPTERS: '/chapters',
-    EMAIL_US: `mailto:${COMPANY_EMAIL}`
-}
+  HOME: "/",
+  LOGIN: "/login",
+  SIGN_UP: "/sign-up",
+  USER_MANAGEMENT: "/usermgmt",
+  RESET_PASSWORD: "/reset-password",
+  ACCOUNT: "/account",
+  BUY: "/buy",
+  FEEDBACK: "/feedback",
+  ORDERS: "/orders",
+  CHAPTERS: "/chapters",
+  EMAIL_US: `mailto:${COMPANY_EMAIL}`
+};
 
 export const PAGE_TITLES = {
-    '/': 'Home',
-    '/login': 'Log In',
-    '/sign-up': 'Sign In',
-    '/usermgmt': 'User Management',
-    '/reset-password': 'Reset Password',
-    '/account': 'Account',
-    '/buy': 'Buy',
-    '/feedback': 'Feedback',
-    '/orders': 'Orders',
-    '/chapters': 'Chapters'
-}
+  "/": "Home",
+  "/login": "Log In",
+  "/sign-up": "Sign In",
+  "/usermgmt": "User Management",
+  "/reset-password": "Reset Password",
+  "/account": "Account",
+  "/buy": "Buy",
+  "/feedback": "Feedback",
+  "/orders": "Orders",
+  "/chapters": "Chapters"
+};
 
 export const THEMES = {
-    DARK: 'dark',
-    LIGHT: 'light'
-}
+  DARK: "dark",
+  LIGHT: "light"
+};
 
 const CHAPTER_NAME = {
-    HABITS: 'habits',
-    PRODUCT: 'product',
-    FIANANCES: 'finances'
-}
+  HABITS: "habits",
+  PRODUCT: "product",
+  FIANANCES: "finances",
+  STRATEGY: "strategy"
+};
 
 export const CHAPTER_ORDER = [
-    CHAPTER_NAME.HABITS,
-    CHAPTER_NAME.PRODUCT,
-    CHAPTER_NAME.FIANANCES
-]
+  CHAPTER_NAME.HABITS,
+  CHAPTER_NAME.PRODUCT,
+  CHAPTER_NAME.FIANANCES,
+  CHAPTER_NAME.STRATEGY
+];
 
-export const PROCESSING_FEE_RATE = 0.3
+export const PROCESSING_FEE_RATE = 0.3;
