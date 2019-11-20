17c37b395ee963c03014f83efc4a03229ac06e47
Raymond427 <rferrel427@gmail.com>
2019-11-19 22:23:13 -0500

    filter chapters that don't have homepage content yet

--- src/component/provider/ChapterProvider.js
+++ src/component/provider/ChapterProvider.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getChapters, performanceMonitor, MAX_ATTRIBUTE_VALUE_LENGTH } from '../../firebase'
 import { addPurchasedProp, compareChapterNames } from '../../utils'
+import homePageImages from '../../data/homePageImages.json'
 
 export const { Provider, Consumer } = React.createContext()
 export const ChapterConsumer = Consumer
@@ -17,9 +18,10 @@ const ChapterProvider = ({ user, orders, children }) => {
                         chaptersFetchTrace.putAttribute('numOfChapters', `${snapShot.docs.length}`)
                         chaptersFetchTrace.putAttribute('result', 'success')
                         setChapters(
-                            snapShot.docs.map(
-                                doc => addPurchasedProp(user, orders, { id: doc.id, ...doc.data() })
-                            ).sort(compareChapterNames)
+                            snapShot.docs
+                                .filter(chapter => homePageImages[chapter.data().name])
+                                .map(doc => addPurchasedProp(user, orders, { id: doc.id, ...doc.data() }))
+                                .sort(compareChapterNames)
                         )
                     }
                 )
