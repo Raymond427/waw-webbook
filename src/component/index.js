6295d30b744b98d5d4886becaa234b7bf87dd3d0
Raymond427 <rferrel427@gmail.com>
2019-11-27 21:30:43 -0500

    make usable offline

--- src/component/index.js
+++ src/component/index.js
@@ -15,63 +15,70 @@ import UserManagementRoute from './routing/UserManagementRoute'
 import ThemeProvider from './provider/ThemeProvider'
 import UserChapterAndOrderProvider from './provider/UserChapterAndOrderProvider'
 import { PATHS } from '../utils/constants'
+import OnlineProvider, { OnlineContext } from './provider/OnlineProvider'
 
 const App = () => (
     <div>
         <ThemeProvider>
-            <UserChapterAndOrderProvider>
-                {({ user, orders, chapters }) => (
-                    <Switch>
-                        <Route exact path={PATHS.HOME}>
-                            <Home chapters={chapters} />
-                        </Route>
-                        <Route exact path={PATHS.LOGIN}>
-                            <Login user={user} />
-                        </Route>
-                        <Route exact path={PATHS.SIGN_UP}>
-                            <Login user={user} />
-                        </Route>
-                        <UserManagementRoute path={PATHS.USER_MANAGEMENT} />
-                        <ProtectedRoute
-                            path={PATHS.RESET_PASSWORD}
-                            condition={!user}
-                            Component={ResetPassword}
-                        />
-                        <ProtectedRoute
-                            path={PATHS.ACCOUNT}
-                            condition={user}
-                            user={user}
-                            Component={Account}
-                        />
-                        <ProtectedRoute
-                            path={`${PATHS.BUY}/:productName`}
-                            condition={user}
-                            user={user}
-                            chapters={chapters}
-                            Component={Payment}
-                        />
-                        <ProtectedRoute
-                            path={PATHS.FEEDBACK}
-                            condition={user}
-                            user={user}
-                            Component={Feedback}
-                        />
-                        <ProtectedRoute
-                            path={PATHS.ORDERS}
-                            condition={user}
-                            user={user}
-                            orders={orders}
-                            Component={Orders}
-                        />
-                        <ChapterRoute
-                            path={`${PATHS.CHAPTERS}/:chapterName`}
-                            Component={Chapter}
-                            chapters={chapters}
-                            user={user}
-                        />
-                    </Switch>
-                )}
-            </UserChapterAndOrderProvider>
+            <OnlineProvider>
+                <OnlineContext.Consumer>
+                    {({ online }) => (
+                        <UserChapterAndOrderProvider>
+                            {({ user, orders, chapters }) => (
+                                <Switch>
+                                    <Route exact path={PATHS.HOME}>
+                                        <Home chapters={chapters} />
+                                    </Route>
+                                    <Route exact path={PATHS.LOGIN}>
+                                        <Login user={user} />
+                                    </Route>
+                                    <Route exact path={PATHS.SIGN_UP}>
+                                        <Login user={user} />
+                                    </Route>
+                                    <UserManagementRoute path={PATHS.USER_MANAGEMENT} />
+                                    <ProtectedRoute
+                                        path={PATHS.RESET_PASSWORD}
+                                        condition={!user}
+                                        Component={ResetPassword}
+                                    />
+                                    <ProtectedRoute
+                                        path={PATHS.ACCOUNT}
+                                        condition={user}
+                                        user={user}
+                                        Component={Account}
+                                    />
+                                    <ProtectedRoute
+                                        path={`${PATHS.BUY}/:productName`}
+                                        condition={user}
+                                        user={user}
+                                        chapters={chapters}
+                                        Component={Payment}
+                                    />
+                                    <ProtectedRoute
+                                        path={PATHS.FEEDBACK}
+                                        condition={user}
+                                        user={user}
+                                        Component={Feedback}
+                                    />
+                                    <ProtectedRoute
+                                        path={PATHS.ORDERS}
+                                        condition={user}
+                                        user={user}
+                                        orders={orders}
+                                        Component={Orders}
+                                    />
+                                    <ChapterRoute
+                                        path={`${PATHS.CHAPTERS}/:chapterName`}
+                                        Component={Chapter}
+                                        chapters={chapters}
+                                        user={user}
+                                    />
+                                </Switch>
+                            )}
+                        </UserChapterAndOrderProvider>
+                    )}
+                </OnlineContext.Consumer>
+            </OnlineProvider>
         </ThemeProvider>
     </div>
 )
---
+++ src/component/provider/OnlineProvider.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+
+export const OnlineContext = React.createContext()
+
+const OnlineProvider = ({ children }) => {
+    const [ online, setOnline ] = useState(null)
+
+    const isOnline = () => setOnline(true)
+    const isOffline = () => setOnline(false)
+
+    useEffect(() => {
+        window.addEventListener("online", isOnline)
+        window.addEventListener("offline", isOffline)
+    
+        return () => {
+          window.removeEventListener("online", isOnline)
+          window.removeEventListener("offline", isOffline)
+        }
+      }, [])
+
+    return (
+        <OnlineContext.Provider value={{ online }}>
+            {children}
+        </OnlineContext.Provider>
+    )
+}
+
+export default OnlineProvider
